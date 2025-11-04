af6a0477f06377b9f8d761ae3fd3ac57c3ff12fb
Tushar Bhatia <138520258+tusharx0809@users.noreply.github.com>
2025-11-04 14:15:30 +0530

    Create Main.java

---
+++ 630A/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+//tusharx0809
+//Problem 630A
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> strs = new ArrayList<String>();
+        long t = sc.nextLong();
+        System.out.println(25);
+    }
+}
