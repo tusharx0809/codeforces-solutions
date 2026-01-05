4d8dacafdf7425f881947b352c38c3a32d218463
Tushar Bhatia <138520258+tusharx0809@users.noreply.github.com>
2026-01-05 13:39:03 +0530

    Create Main.java

---
+++ 2044A/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//tusharx0809
+//Problem 2044A
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc != 0){
+            int testCase = sc.nextInt();
+            System.out.println(testCase-1);
+            tc--;
+        }
+    }
+}
